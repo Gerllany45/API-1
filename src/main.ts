124d9da93dac294bec752d723efdaced9c9aec76
Gerllany45 <156119036+Gerllany45@users.noreply.github.com>
2024-01-30 17:34:54 -0300

    revisão da aula

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import express from "express";
+import {db,firestore} from '../banco_de_dados/firebase';
 
 const app = express();
 
 app.listen(3000,function () {
     console.log('servicço rodando em http://localhost:3000');
-})
+});
