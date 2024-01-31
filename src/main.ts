9c12bd3edffcab16896a35ebfcd73196ef707721
Gerllany45 <156119036+Gerllany45@users.noreply.github.com>
2024-01-31 17:44:06 -0300

    criacao de rota

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
-import express from "express";
-import {db,firestore} from '../banco_de_dados/firebase';
+import express from 'express';
+import { db, firestore } from '../banco_de_dados/firebase';
 
 const app = express();
 
-app.listen(3000,function () {
+app.get('/', (req, res) => {
+    res.send('Bem-vindo')
+});
+
+app.listen(3000, function () {
     console.log('servicço rodando em http://localhost:3000');
 });
+
+app.post('/usuario', async (req, res) => {
+    const usuario = req.body.nome
+
+    try {
+        const docRef = await firestore.addDoc(firestore.collection(db, 'usuario'), {
+            nome: nome,
+        })
+        res.send(docRef.id)
+    } catch (e) {
+       console.log(e)
+       res.status(500).send(e)
+    }
+});
+
