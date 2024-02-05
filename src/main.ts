1d2a0b380ab8ac4f7399e1461f40c759b49da9d1
Gerllany45 <156119036+Gerllany45@users.noreply.github.com>
2024-02-05 17:54:38 -0300

    uma nova modificacao

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import { db, firestore } from '../banco_de_dados/firebase';
+import e from 'express';
 
 const app = express();
 app.use(express.json())
@@ -14,19 +15,38 @@ app.listen(3000, function () {
 
 app.post('/usuario', async (req, res) => {
     const nome = req.body.nome
-    const email=req.body.email
-    const telefone= req.body.telefone
+    const email = req.body.email
+    const telefone = req.body.telefone
 
     try {
         const docRef = await firestore.addDoc(firestore.collection(db, 'usuario'), {
             nome: nome,
-            email:email,
-            telefone:telefone
+            email: email,
+            telefone: telefone
         })
         res.send("Usuário  adicionado com sucesso" + docRef.id);
     } catch (e) {
-       console.log(e)
-       res.status(500).send(e)
+        console.log(e)
+        res.status(500).send(e)
     }
 });
+
+app.get('/listarUsuarios', async (req, res) => {
+      try{
+        const usuarios = await firestore.getDocs(firestore.collection(db, 'usuario'))
+
+    
+
+        const usuariosLista = usuarios.docs.map((doc) => ({
+            id:doc.id,
+           ...doc.data(),
+        }))
+    
+        res.send(usuariosLista)
+        }catch(e){
+         console.log("erro ao listar usuarios" + e)
+         res.status(500).send("erro ao listar usuarios:"+ e)
+      }
+
+})
 
